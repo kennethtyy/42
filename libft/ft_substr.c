f223c740baee9900bec7c76eed93b1b3e301c8bd
Kenneth Tan <kentan@c2r2s15.42singapore.sg>
2023-10-15 17:57:43 +0800

    fixed all - no bonus

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -16,15 +16,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*sub_s;
 	size_t	i;
 
-	if (!s)
-		return (0);
 	if (start > ft_strlen(s))
-		return (ft_strdup(""));
-	i = 0;
-	sub_s = (char *) malloc (sizeof(char) * len + 1);
-	if (!sub_s)
+		len = 0;
+	if (ft_strlen(s) - start >= len)
+		sub_s = (char *) malloc (sizeof(char) * len + 1);
+	else
+		sub_s = (char *) malloc (sizeof(char) * (ft_strlen(s) - start) + 1);
+	if (!s || !sub_s)
 		return (0);
-	while (i < len)
+	i = 0;
+	while (i < len && i < ft_strlen(s))
 	{
 		sub_s[i] = s[start + i];
 		i++;
